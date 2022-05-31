3f7208eb2f1a0d21e865129c8c76dea619695687
Shantanu <shantanu.sharan10@yahoo.com>
2022-06-01 00:17:46 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Counterfunction from './Counterfunction';
+
 import './App.css';
+import Counterclass from './Counterclass';
 
 function App() {
   return (
     <div className="App">
      <Counterfunction/>
+     <Counterclass/>
     </div>
   );
 }
